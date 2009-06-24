93d2a5e47ac02be428bba660684f2aea0b4b926c
Tomas Pollak <tomas@bootic.net>
2009-06-24 01:01:42 -0400

    Fixed Debian Package generator so that we can put it in any descendant directory

--- distribution/linux/make_deb.sh
+++ distribution/linux/make_deb.sh
@@ -1,27 +1,36 @@
 #!/bin/sh
 # Debian Package Builder, by Tomas Pollak
 
+cwd=`pwd`
+
+while [ ! -f prey.sh ]; do
+	cd ..
+done
+
+basedir=`pwd`
+cd $cwd
+
 # Remove temporary directory
 rm -rf ./build
 rm *.deb 2> /dev/null
 
 # Configure control folder and file
-
 mkdir -p ./build/tmp/prey
 cp -r DEBIAN ./build/tmp/prey/
 
+# copying the base dir stuff
 mkdir -p ./build/tmp/prey/usr/share/prey
-cp ../prey.sh ./build/tmp/prey/usr/share/prey/
-cp ../config ./build/tmp/prey/usr/share/prey/
-cp ../configure.py ./build/tmp/prey/usr/share/prey/
-cp -r ../lang ./build/tmp/prey/usr/share/prey/
-cp -r ../platform ./build/tmp/prey/usr/share/prey/
-cp -r ../modules ./build/tmp/prey/usr/share/prey/
-cp -r ../pixmaps ./build/tmp/prey/usr/share/prey/
+cp $basedir/prey.sh ./build/tmp/prey/usr/share/prey/
+cp $basedir/config ./build/tmp/prey/usr/share/prey/
+cp $basedir/configure.py ./build/tmp/prey/usr/share/prey/
+cp -r $basedir/lang ./build/tmp/prey/usr/share/prey/
+cp -r $basedir/platform ./build/tmp/prey/usr/share/prey/
+cp -r $basedir/modules ./build/tmp/prey/usr/share/prey/
+cp -r $basedir/pixmaps ./build/tmp/prey/usr/share/prey/
 
-cp ../CHANGELOG ./build/tmp/prey/usr/share/prey/
-cp ../LICENSE ./build/tmp/prey/usr/share/prey/
-cp ../README ./build/tmp/prey/usr/share/prey/
+cp $basedir/CHANGELOG ./build/tmp/prey/usr/share/prey/
+cp $basedir/LICENSE ./build/tmp/prey/usr/share/prey/
+cp $basedir/README ./build/tmp/prey/usr/share/prey/
 
 # lets copy the config shortcut
 mkdir -p ./build/tmp/prey/usr/share/applications
