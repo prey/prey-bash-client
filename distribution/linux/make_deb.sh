0d592a7dfc2969b76ba297dc5f9e1bb43b77d649
Tomas Pollak <tomas@bootic.net>
2009-06-26 05:12:59 -0400

    Fixed KDialog in Debian post install. Set up right permissions for security. Fixed removal of files. Finished (encoded) password setting with GUI configurator. Gotta sleep now.

--- distribution/linux/make_deb.sh
+++ distribution/linux/make_deb.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Debian Package Builder, by Tomas Pollak
 
+if [ `whoami` != 'root' ]; then
+	echo 'Since we need to set up root permissions you need to run this as root. A simple "sudo" will do. :)'
+	exit
+fi
+
 cwd=`pwd`
 
 while [ ! -f prey.sh ]; do
@@ -10,7 +15,7 @@ done
 basedir=`pwd`
 cd $cwd
 
-# Remove temporary directory
+# Remove temporary directory and .DEBs
 rm -rf ./build
 rm *.deb 2> /dev/null
 
@@ -23,6 +28,7 @@ mkdir -p ./build/tmp/prey/usr/share/prey
 cp $basedir/prey.sh ./build/tmp/prey/usr/share/prey/
 cp $basedir/config ./build/tmp/prey/usr/share/prey/
 cp $basedir/configure.py ./build/tmp/prey/usr/share/prey/
+cp -r $basedir/lib ./build/tmp/prey/usr/share/prey/
 cp -r $basedir/lang ./build/tmp/prey/usr/share/prey/
 cp -r $basedir/platform ./build/tmp/prey/usr/share/prey/
 cp -r $basedir/modules ./build/tmp/prey/usr/share/prey/
@@ -36,10 +42,12 @@ cp $basedir/README ./build/tmp/prey/usr/share/prey/
 mkdir -p ./build/tmp/prey/usr/share/applications
 cp prey-config.desktop ./build/tmp/prey/usr/share/applications
 
+sudo chown root.root ./build/tmp/prey/usr/share/prey -R
+
 # Make the deb package
 dpkg-deb -b ./build/tmp/prey ./build
 
 mv build/*.deb .
 # Remove temporary directory
-rm -rf ./build
+sudo rm -rf ./build
 
