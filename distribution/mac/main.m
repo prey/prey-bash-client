fc6b815834109497907d12f92fc36c5558ae4b1c
Tomás Pollak <tomaspollak@gmail.com>
2010-03-04 17:05:45 -0300

    Moving distribution to a different repo

--- distribution/mac/main.m
+++
@@ -1,9 +0,0 @@
-extern void ASKInitialize();
-extern int NSApplicationMain(int argc, const char *argv[]);
-
-int main(int argc, const char *argv[])
-{
-    ASKInitialize();
-
-    return NSApplicationMain(argc, argv);
-}
