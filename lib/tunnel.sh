d69417c35ba89ae57989b39bdd9fb5435acf6605
Tomás Pollak <tomaspollak@gmail.com>
2010-10-08 14:48:37 -0400

    Added SSH Tunnel support, and initial stuff to support S3 get/put

---
+++ lib/tunnel.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# Auto SSH Tunnel script, by Tomas Pollak
+# ./tunnel.sh [user] [pass] [host] [local_port] [remote_port]
+
+# starting_port=$4
+#get_port(){
+#	search_ports=10
+#	port=$starting_port
+
+#	while [ -z "`telnet $3 $port < /dev/null 2>&1 | grep Connected`" ]; do
+#		port=`expr $port + 1`
+#		echo " -- Probing for connection on port $port..."
+#		if [ $port -eq $(($starting_port+$search_ports)) ]; then
+#			port=0
+#			break
+#		fi
+#	done
+# }
+
+trap cleanup_tunnel EXIT
+
+cleanup_tunnel(){
+	rm -Rf "$askfile" 2> /dev/null
+}
+
+cwd=`pwd`
+export SSH_ASKPASS="$askfile"
+export SSH_TTY=/dev/null
+# export DISPLAY=none:0.0
+
+askfile="$cwd/return"
+cat > "$askfile" << END
+#!/bin/sh
+echo $2
+END
+chmod 700 "$askfile"
+
+# eval `ssh-agent` >/dev/null
+ssh-add < /dev/null
+
+# echo " -- Connecting to $host_port..."
+ssh -N -o 'ExitOnForwardFailure=yes' -R ${5}:localhost:${4} ${1}@${3} &
+tunnel_pid=$!
+
+sleep 5
+if [ "`ps -p $tunnel_pid | grep $tunnel_pid`" ]; then
+	echo "$tunnel_pid" > "/tmp/reverse_tunnel.pid"
+fi
