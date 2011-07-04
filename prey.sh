d64e535c68ee5ca6210e25c5b3947078fbb575d0
Tomás Pollak <tomaspollak@gmail.com>
2011-07-04 18:40:45 -0400

    Added running_from_cron function to check if we really are

--- prey.sh
+++ prey.sh
@@ -105,7 +105,7 @@ fi
 ####################################################################
 
 # only do this if Prey is being run from cron in Mac and Linux
-if [[ ! -t 1 && "$os" != "windows" && "$post_method" == "http" ]]; then
+if [[ "$os" != "windows" && -n "$(running_from_cron)" && "$post_method" == "http" ]]; then
 	seconds_to_wait=$(get_random_number 59)
 	log " -- Pausing for ${seconds_to_wait} seconds..."
 	sleep $seconds_to_wait
