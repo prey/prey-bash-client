aaa20b45dd59a32c5f6609dae973b3b88cb9161c
Jeff <jeff5046@gmail.com>
2011-09-22 19:08:36 -0400

    changes

--- platform/linux/prey-config.py
+++ platform/linux/prey-config.py
@@ -6,7 +6,7 @@
 ################################################
 
 # if having trouble with the GTK theme as root, do this:
-# sudo ln -s ~/.themes/ /root/.themes
+# ln -s /home/<your_username>/.themes/ /root/.themes
 
 ################################################
 # base includes
